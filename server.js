f012745187beb607d8688f461ebb68f69574bb5b
Utkarsh Kumar <kumarutkarsh2505@gmail.com>
2022-03-14 21:14:00 +0530

    put

--- server.js
+++ server.js
@@ -2,8 +2,24 @@ const express = require("express");
 const PORT = 8000;
 const address = require("./db.json");
 let app = express();
-app.get("/",(req,res)=>{
+
+app.use(express.json());
+
+app.get("/addresses",(req,res)=>{
 res.json(address)
+})
+
+app.post("/addresses",(req,res)=>{
+address.push(req.body);
+res.json(req.body);
+})
+
+app.put("/addresses/:id",(req,res)=>{
+    const {id} = req.params;
+    const userBody = req.body;
+    const updateAddress = address.map((item)=>(item.id === id ? item : userBody))
+    res.status(200).json(updateAddress);
+
 })
 
 app.listen(PORT,()=>{
